695c12370d97d69e35454d3b9f13e87500f165de
Alcsaw <augustolcschnorr2@gmail.com>
2019-06-22 20:45:53 -0300

    ajusta gitignore

--- Main.py
+++ Main.py
@@ -5,3 +5,31 @@ Limitations:
   TODO: Add decryption option
         Add verbose option for running scripts
 '''
+
+from Crypto.Cipher import AES
+from Crypto.Cipher import DES
+
+key = "testtesttesttest"
+
+iv = "testtesttesttest"
+
+print("encrypting", )
+
+aes = AES.new(key, AES.MODE_CBC, iv)
+plaintext = 'hello world 1234'
+encd = aes.encrypt(plaintext)
+print("AES", encd)
+
+key = "testtest"
+iv = "testtest"
+#des = DES.new(key, DES.MODE_CBC)
+des = DES.new(key, DES.MODE_OFB, iv)
+#cipher = DES.new(key, DES.MODE_OFB)
+#plaintext = 'hello world 1234'
+plaintext = key
+#encd = des.encrypt(plaintext)
+
+encd = des.decrypt(plaintext)
+print("DES", encd)
+
+#msg = cipher.iv + cipher.encrypt(plaintext)
