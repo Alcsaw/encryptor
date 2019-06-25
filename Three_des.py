e152fba4996bd0c7b55bbe04b6ebd256bed8ed87
evilmaax <maximilianomeyer48@gmail.com>
2019-06-24 21:36:24 -0300

    Oi :)

--- Three_des.py
+++ Three_des.py
@@ -1,4 +1,5 @@
 from Crypto.Cipher import DES3
+from Crypto.Util.Padding import unpad
 
 BLOCK_SIZE = 8
 iv = b'\xbd\xbe"r\xca\x19"\xdd'
@@ -11,6 +12,7 @@ def ecb(chave, msg, op):
         return msg
     else:
         decifra = DES3.new(chave, DES3.MODE_ECB)
+        decifra = unpad(decifra.decrypt(msg), BLOCK_SIZE).decode("utf-8")
         return decifra
 
 
@@ -21,6 +23,7 @@ def cbc(chave, msg, op):
         return msg
     else:
         decifra = DES3.new(chave, DES3.MODE_CBC, IV=iv)
+        decifra = unpad(decifra.decrypt(msg), BLOCK_SIZE).decode("utf-8")
         return decifra
 
 def cfb(chave, msg, op):
@@ -30,6 +33,7 @@ def cfb(chave, msg, op):
         return msg
     else:
         decifra = DES3.new(chave, DES3.MODE_CFB, IV=iv)
+        decifra = unpad(decifra.decrypt(msg), BLOCK_SIZE).decode("utf-8")
         return decifra
 
 
@@ -40,6 +44,7 @@ def ofb(chave, msg, op):
         return msg
     else:
         decifra = DES3.new(chave, DES3.MODE_OFB, IV=iv)
+        decifra = unpad(decifra.decrypt(msg), BLOCK_SIZE).decode("utf-8")
         return decifra
 
 
@@ -50,4 +55,5 @@ def ctr(chave, msg, op):
         return msg
     else:
         decifra = DES3.new(chave, DES3.MODE_CTR, nonce=b'', initial_value=iv)
+        decifra = unpad(decifra.decrypt(msg), BLOCK_SIZE).decode("utf-8")
         return decifra
